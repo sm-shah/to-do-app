3e6fc0b6322f2df942735b8964502f5fcc9288ee
sm-shah <siddharthshah870@gmail.com>
2022-06-16 00:26:17 +0530

    1 file modified

--- src/app/to-do/component/to-do-list/to-do-list.component.ts
+++ src/app/to-do/component/to-do-list/to-do-list.component.ts
@@ -3,18 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToDoService } from '../../service/to-do.service';
 
-const ELEMENT_DATA = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-to-do-list',
@@ -24,7 +12,7 @@ const ELEMENT_DATA = [
 })
 export class ToDoListComponent implements OnInit {
   displayedColumns: string[] = ['Title', 'Notes', 'Date','Edit'];
-  dataSource = ELEMENT_DATA;
+  dataSource = null;
   list: any[]=[];
 
   constructor(private router:Router,private toDoService: ToDoService) { }
